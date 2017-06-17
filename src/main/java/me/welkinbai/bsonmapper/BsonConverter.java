cd46475393eb7935442a94ee6b84c0bed06ba32b
welkinbai <xwmbxx@126.com>
2017-06-17 15:43:16 +0800

    BsonConverter commment

--- src/main/java/me/welkinbai/bsonmapper/BsonConverter.java
+++ src/main/java/me/welkinbai/bsonmapper/BsonConverter.java
@@ -1,6 +1,7 @@
 package me.welkinbai.bsonmapper;
 
 /**
+ * BsonConverter's implementation class must be stateless and thread-safe
  * Created by welkinbai on 2017/3/26.
  */
 public interface BsonConverter {
